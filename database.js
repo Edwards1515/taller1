718e6c327bda032a892527a231c62ca261ced6f0
edwards1515 <edwards1515@hotmail.com>
2019-11-21 00:54:03 -0500

    todo lo puedo en cristo que me fortalece

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = "mongodb://jj_lizarazor:ananke123@clustera-shard-00-00-eliiu.mongodb.net:27017,clustera-shard-00-01-eliiu.mongodb.net:27017,clustera-shard-00-02-eliiu.mongodb.net:27017/tallerDb?ssl=true&replicaSet=ClusterA-shard-0&authSource=admin&retryWrites=true&w=majority";
+const uri = "mongodb://primerUsuario:ed25797059@cluster0-shard-00-00-dsvka.mongodb.net:27017,cluster0-shard-00-01-dsvka.mongodb.net:27017,cluster0-shard-00-02-dsvka.mongodb.net:27017/ananke-examen?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true })
     .then(db => { console.log('Conexión exitosa con cluster MongoDB: Atlas') })
